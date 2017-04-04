a829adb21afbf3ca3ba2691c961e2b75ab1f5236
Martín González <martok22@users.noreply.github.com>
2017-04-04 12:20:17 -0300

    Add files via upload

---
+++ Clase 3/Clase 3/Tiempo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_3
+{
+    class Tiempo
+    {
+        public int cantidad;
+
+        public Tiempo(int cantidad)
+        {
+            this.cantidad = cantidad;
+        }
+    }
+}
