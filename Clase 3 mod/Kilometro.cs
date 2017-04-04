eb4d61d4ef4869592471d817207d35a4afbb3829
Martín González <martok22@users.noreply.github.com>
2017-04-04 04:02:54 -0300

    ejercicio #2, clase #4

---
+++ Clase 3 mod/Kilometro.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3
+{
+    class Kilometro
+    {
+       int cantidad;
+	   
+	   public Kilometro(int cantidad)
+	   {
+			this.cantidad = cantidad;
+	   }
+	}
+	   
+}
---
+++ Clase 3 mod/Tiempo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3
+{
+    class Tiempo
+    {
+       int cantidad;
+	   
+	   public Tiempo(int cantidad)
+	   {
+			this.cantidad = cantidad;
+	   }
+	}
+	   
+}
