bd4f83d9db91909472d8c66c5b854390ca8b288e
Martín González <martok22@users.noreply.github.com>
2017-04-18 01:16:05 -0300

    Clase 8

---
+++ Entidades/ConsoleArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Indice;
+            Entidades.Aula MiAula=new Entidades.Aula();
+
+            for (int ContadorDeLegajo = 0; ContadorDeLegajo < 11; ContadorDeLegajo++)
+            {                
+            Entidades.Alumno NuevoAlumno = new Entidades.Alumno("Jarkodeo", "Furioso", ContadorDeLegajo);
+            MiAula.AgregarAlumno(NuevoAlumno);
+            }
+
+            Entidades.Alumno borrado = new Entidades.Alumno("Jarkodeo", "Furioso", 6);
+
+            MiAula.BorrarAlumno(borrado);
+
+            
+            //Indice = MiAula.ObtenerLugarLibre();
+        
+        }
+    }
+}
---
+++ Entidades/Entidades/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Alumno
+    {
+        private string Apellido;
+        private string Nombre;
+        private int Legajo;
+
+        public Alumno(string apel, string nom, int leg)
+        {
+            this.Apellido = apel;
+            this.Nombre = nom;
+            this.Legajo = leg;
+        }
+
+        public static bool operator ==(Alumno alumnoX, Alumno alumnoY)
+        {
+            return (alumnoX.Apellido == alumnoY.Apellido && alumnoX.Legajo == alumnoY.Legajo);
+        }
+        public static bool operator !=(Alumno alumnoX, Alumno alumnoY)
+        {
+            return !(alumnoX==alumnoY);
+        }
+    }
+}
