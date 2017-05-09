b280a620a4310b5b9d56e3f09e82561eb121431c
Martín González <martok22@users.noreply.github.com>
2017-05-09 12:17:41 -0300

    Add files via upload

---
+++ Parcial/Consola2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepasoAbstracta;
+
+namespace Consola2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class3 clase3 = new Class3();
+        }
+    }
+}
