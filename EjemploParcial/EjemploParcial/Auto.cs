214e8b3148e387dde8a987c4607ce22b3ee4c9bc
Martín González <martok22@users.noreply.github.com>
2017-05-17 01:27:30 -0300

    Add files via upload

---
+++ EjemploParcial/EjemploParcial/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploParcial
+{
+    public class Auto : Vehiculo
+    {
+        private int _cantidadDePuertas;
+
+        public Auto(string patente, int cantidadDePuertas) : base(patente)
+        {
+            this._cantidadDePuertas = cantidadDePuertas;
+        }
+    }
+}
---
+++ EjemploParcial/EjemploParcial/Moto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploParcial
+{
+    public class Moto : Vehiculo
+    {
+        private int _cilindrada;
+
+        public Moto(string patente, int cilindrada) : base(patente)
+        {
+            this._cilindrada = cilindrada;
+        }
+    }
+}
